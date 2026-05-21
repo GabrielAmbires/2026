3211efc536f5259dd21b31068c70b56395b31789
GabrielAmbires <gabriel.a.oliveira31@aluno.senai.br>
2026-05-21 08:58:24 -0300

    .

--- Quiz Temático/script.js
+++ Quiz Temático/script.js
@@ -15,13 +15,49 @@ let perguntas = [
         pergunta: "Qual o nome desse carro?",
         imagem: "img/lambo.png",
         respostas: ["Lamborghini Huracán", "Lamborghini Aventador", "Lamborghini Revuelto", "Lamborghini Temerario"],
-        correta: 1 //2023
+        correta: 1 //lamborghini Aventador
     },
     {
         pergunta: "Qual o nome desse carro?",
-        imagem: "img/lambo.png",
-        respostas: ["Lamborghini Huracán", "Lamborghini Aventador", "Lamborghini Revuelto", "Lamborghini Temerario"],
-        correta: 1 //2023
+        imagem: "img/bugatti.png",
+        respostas: ["Bugatti Veyron", "Bugatti Chiron", "Bugatti Divo", "Bugatti La Voiture Noire"],
+        correta: 0 //Bugatti Veyron
+    },
+    {
+        pergunta: "Qual o nome desse carro?",
+        imagem: "img/m4-.png",
+        respostas: ["BMW I8", "BMW Série 8", "BMW M4", "BMW M3"],
+        correta: 2, //BMW M4
+    },
+    {
+        pergunta: "De qual carro é essa traseira?",
+        imagem: "img/720s-.png",
+        respostas: ["McLaren Artura", "McLaren 720S", "McLaren 570S", "McLaren P1"],
+        correta: 1, //McLaren 720S
+    },
+    {
+        pergunta: "Qual o traseira é essa?",
+        imagem: "img/porsche-.png",
+        respostas: ["Porsche 911 Turbo S", "Porsche 918", "Porsche 991", "Porsche 992"],
+        correta: 0 //Porsche 911 Turbo S
+    },
+    {
+        pergunta: "Qual o nome desse carro?",
+        imagem: "img/r8-.png",
+        respostas: ["Audi RS7", "Audi A8", "Audi R8", "Audi RS7"],
+        correta: 2 //Audi R8
+    },
+    {
+        pergunta: "Qual o nome desse carro?",
+        imagem: "img/koenigsegg_.png",
+        respostas: ["Koenigsegg Agera", "Koenigsegg Jesko", "Koenigsegg Regera", "Koenigsegg CC8S"],
+        correta: 1 //Koenigsegg Jesko
+    },
+    {
+        pergunta: "Qual o nome desse carro?",
+        imagem: "img/rolls-royce.png",
+        respostas: ["Rolls-Royce Phantom", "Rolls-Royce Ghost", "Rolls-Royce Cullinan", "Rolls-Royce Dawn"],
+        correta: 2 //Rolls-Royce Cullinan
     },
 ]
 
