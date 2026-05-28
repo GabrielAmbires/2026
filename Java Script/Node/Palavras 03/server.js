afa96cd7c9fd747bd3a00acc1fbf7155a1f3d1fd
GabrielAmbires <gabriel.a.oliveira31@aluno.senai.br>
2026-05-28 08:52:38 -0300

    .

--- Java Script/Node/Palavras 03/server.js
+++ Java Script/Node/Palavras 03/server.js
@@ -11,3 +11,20 @@ app.use(express.json())
 
 //Procura pelo index.html na pasta public
 app.use(express.static("public"))
+
+//conexão com o banco de dados
+const conexao = mysql.createConnection({
+    host: "localhost", //endereço do banco de dados
+    user: "root", //usuário do banco de dados
+    password: "senai", //senha do banco de dados
+    database: "palavras" //banco de dados
+})
+
+//Testar a conexão com o banco de dados
+conexao.connect((erro) => {
+    if(erro){
+        console.log("Erro na conexão")
+    } else {
+        console.log("Conectado ao MySQL!")
+    }
+})
