5b3bb8f2c4029e187e1b732136d526a0c1de8f93
GabrielAmbires <gabriel.a.oliveira31@aluno.senai.br>
2026-05-28 09:26:09 -0300

    .

--- Java Script/Node/Palavras 03/server.js
+++ Java Script/Node/Palavras 03/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 //criar o servidor usando o express
 const app = express();
 //ativar o cors no projeto
-app.use(cors)
+app.use(cors())
 //permite que os dados resebam o json
 app.use(express.json())
 
